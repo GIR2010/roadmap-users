f4403bcca61f48008ad13952948e328570d04d78
Galikhanov Ildar <irgalikhanov@gmail.com>
2023-05-05 00:39:58 +0300

    Send Post as sub-object

--- src/main/kotlin/com/roadmap/users/controller/UserController.kt
+++ src/main/kotlin/com/roadmap/users/controller/UserController.kt
@@ -1,14 +1,16 @@
 package com.roadmap.users.controller
 
 import com.roadmap.users.entity.User
+import com.roadmap.users.entity.UserModel
 import com.roadmap.users.service.UserService
+import com.roadmap.users.service.PostService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.net.URI
 
 @RestController
 @RequestMapping("/users")
-class UserController(private val userService: UserService) {
+class UserController(private val userService: UserService, private val postService: PostService) {
 
     @GetMapping
     fun getUser(): ResponseEntity<List<User>> {
@@ -23,19 +25,29 @@ class UserController(private val userService: UserService) {
     }
 
     @PostMapping
-    fun createUser(@RequestBody user: User): ResponseEntity<User> {
-        val createdUser = userService.createUser(user)
+    fun createUser(@RequestBody user: UserModel): ResponseEntity<User> {
+        val post = if (user.postId != null) postService.getPostById(user.postId!!) else null
+        val userModel = User(
+            name = user.name,
+            surname = user.surname,
+            secondName = user.secondName,
+            avatar = user.avatar,
+            streamId = user.streamId,
+            post = post
+        )
+        val createdUser = userService.createUser(userModel)
         return ResponseEntity.created(URI.create("/${createdUser.id}")).body(createdUser)
     }
 
     @PutMapping("/{id}")
-    fun updateUser(@PathVariable("id") id: String, @RequestBody user: User): ResponseEntity<User> {
+    fun updateUser(@PathVariable("id") id: String, @RequestBody user: UserModel): ResponseEntity<User> {
         val existingUser = userService.getUserById(id) ?: return ResponseEntity.notFound().build()
+        val post = if (user.postId != null) postService.getPostById(user.postId!!) else null
         existingUser.name = user.name
         existingUser.surname = user.surname
         existingUser.secondName = user.secondName
         existingUser.streamId = user.streamId
-        existingUser.postId = user.postId
+        existingUser.post = post
         existingUser.avatar = user.avatar
         val updatedUser = userService.updateUser(existingUser)
         return ResponseEntity.ok(updatedUser)
